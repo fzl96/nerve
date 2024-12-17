c016551f89f3af34f87273f929276292c7315cf4
fzl96 <fpsdragon333@gmail.com>
2024-12-17 09:17:07 +0700

    feat: Add about us section

--- src/app/tentang-kami/page.tsx
+++ src/app/tentang-kami/page.tsx
@@ -8,6 +8,21 @@ export default function AboutUs() {
           <Section>
             <Container>
               <h1>Tentang Kami</h1>
+              <p>
+                Aplikasi Sistem Pakar Diagnosa Penyakit Carpal Tunnel Syndrome
+                Dengan Metode Forward Chaining Berbasis Web ini dibuat untuk
+                memenuhi tugas akhir perkuliahan dan sebagai salah satu
+                persyaratan untuk memperoleh gelar Sarjana di Program Studi
+                Teknik Informatika Fakultas Teknik Universitas Riau
+              </p>
+              <h2>Contact</h2>
+              <p className="flex flex-col">
+                Nama: Encik Muhammad Surya,
+                <span>
+                  Email:{" "}
+                  <a href="mailto:encikms18@gmail.com">encikms18@gmail.com</a>
+                </span>
+              </p>
             </Container>
           </Section>
         </Container>
