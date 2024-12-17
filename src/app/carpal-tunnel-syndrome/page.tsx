4705e1dcde10af43049407c1c4eb0fd60c44ac78
fzl96 <fpsdragon333@gmail.com>
2024-12-17 09:21:51 +0700

    feat: Add information section

--- src/app/carpal-tunnel-syndrome/page.tsx
+++ src/app/carpal-tunnel-syndrome/page.tsx
@@ -8,6 +8,38 @@ export default function AboutUs() {
           <Section>
             <Container>
               <h1>Informasi Penyakit</h1>
+              <h2>Apa itu Carpal Tunnel Syndrome?</h2>
+              <p>
+                Carpal Tunnel Syndrome (CTS) adalah kondisi yang terjadi akibat
+                tekanan pada saraf median, yang berjalan melalui terowongan
+                karpal di pergelangan tangan. Saraf ini bertanggung jawab untuk
+                memberikan sensasi dan mengontrol sebagian gerakan di tangan,
+                terutama pada ibu jari, jari telunjuk, jari tengah, dan sebagian
+                jari manis.
+              </p>
+              <h2>Penyebab Carpal Tunnel Syndrome</h2>
+              <p>
+                CTS disebabkan oleh tekanan berlebihan pada saraf median, yang
+                bisa dipicu oleh:
+              </p>
+              <ul>
+                <li>
+                  Gerakan Repetitif: Aktivitas seperti mengetik, menggunakan
+                  mouse, atau pekerjaan manual lainnya.
+                </li>
+                <li>
+                  Cedera atau Pembengkakan: Cedera pada pergelangan tangan atau
+                  kondisi seperti arthritis.
+                </li>
+                <li>
+                  Kehamilan: Perubahan hormonal selama kehamilan dapat
+                  menyebabkan pembengkakan di terowongan karpal.
+                </li>
+                <li>
+                  Kondisi Medis: Penyakit seperti diabetes, hipotiroidisme, atau
+                  obesitas.
+                </li>
+              </ul>
             </Container>
           </Section>
         </Container>
